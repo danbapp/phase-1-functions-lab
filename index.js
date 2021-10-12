94b8ddd2105a1f70d85fb6d2b1c5ed043e2a5296
Daniel <danbapp@gmail.com>
2021-10-12 13:57:27 -0400

    commit tree 8ccf5cb59804436392352b4542685c123b36cfd2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+  if(street > 42){
+    return street - 42;}
+  else if(street < 42){
+    return 42 - street;
+  }
+  }
+
+function distanceFromHqInFeet(street){
+  let distanceInFeet = distanceFromHqInBlocks(street);
+  return distanceInFeet * 264;
+  }
+
+function distanceTravelledInFeet(start, end){
+  if(start > end){
+    return (start - end) * 264;}
+  else if(start < end){
+    return (end - start) * 264;}
+}  
+
+function calculatesFarePrice(start, destination){
+  let totalFeet = distanceTravelledInFeet(start, destination);
+  if (totalFeet > 0 && totalFeet <= 400){
+    return 0;}
+  else if(totalFeet > 400 && totalFeet < 2000){
+    return (totalFeet - 400) * .02;}
+  else if(totalFeet > 2000 && totalFeet <= 2500){
+    return 25;}
+  else {
+      return 'cannot travel that far';}
+  }
